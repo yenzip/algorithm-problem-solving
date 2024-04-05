56a64c9140007ed7fa8d43968bbc0d8cb097330f
Yerim Eom <djadpfa1002@naver.com>
2024-04-06 00:43:35 +0900

    [Gold V] Title: 강의실 배정, Time: 704 ms, Memory: 68916 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -5,35 +5,41 @@ class Lecture {
     int S;
     int T;
     
-    Lecture(int S, int T) {
+    public Lecture(int S, int T) {
         this.S = S;
         this.T = T;
     }
 }
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        Lecture[] lectures = new Lecture[N];
-        for (int i = 0; i < N; ++i) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            lectures[i] = new Lecture(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        }
-        
-        Arrays.sort(lectures, (a, b) -> Integer.compare(a.S, b.S));
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(Lecture lecture : lectures) {
-            if(!pq.isEmpty() && pq.peek() <= lecture.S) {
-                pq.poll();
-            }
-            pq.offer(lecture.T);
-        }
-        
-        System.out.println(pq.size());
-        br.close();
+    public static void main(String args[]) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int N = Integer.parseInt(br.readLine());
+      Lecture[] lectures = new Lecture[N];
+      
+      for(int i = 0; i < N; i++) {
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          
+          int S = Integer.parseInt(st.nextToken());
+          int T = Integer.parseInt(st.nextToken());
+          
+          lectures[i] = new Lecture(S, T);
+      }
+      
+      Arrays.sort(lectures, (o1, o2) -> o1.S - o2.S);
+      
+      PriorityQueue<Integer> pq = new PriorityQueue<>();    // 종료시간을 오름차순으로 정렬한 우선순위 큐
+      
+      for(int i = 0; i < N; i++) {
+          if(!pq.isEmpty() && pq.peek() <= lectures[i].S) {
+              pq.poll();
+          }
+          pq.offer(lectures[i].T);
+      }
+      
+      System.out.println(pq.size());
+      
+      br.close();
     }
 }
