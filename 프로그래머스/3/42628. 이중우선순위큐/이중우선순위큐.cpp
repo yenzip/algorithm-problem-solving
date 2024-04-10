36392eeef9f2fd331c1dcde0bdab9c7c3755b099
Yerim Eom <djadpfa1002@naver.com>
2024-04-10 16:35:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    vector<int> answer;
-    priority_queue<int, vector<int>, greater<int>> pq;  // 최소힙
-    deque<int> dq;
+    priority_queue<int> pq;  // 최대힙
     
     for(string op : operations) {
         char cmd = op[0];
@@ -17,35 +15,30 @@ vector<int> solution(vector<string> operations) {
                 continue;
             }
             
-            while(!pq.empty()) {
-                dq.push_back(pq.top());
-                pq.pop();
-            }
-            
             if(val == 1) {  // 최댓값 삭제
-                dq.pop_back();
+                pq.pop();
             } else {        // 최솟값 삭제
-                dq.pop_front();
-            }
-            
-            while(!dq.empty()) {
-                pq.push(dq.front());
-                dq.pop_front();
+                priority_queue<int> tmp;
+                
+                while(pq.size() > 1) {
+                    tmp.push(pq.top());
+                    pq.pop();
+                }
+                
+                pq = tmp;
             }
         }
     }
     
-    while(!pq.empty()) {
-        dq.push_back(pq.top());
+    if(pq.empty()) {
+        return vector<int>{0, 0};
+    }
+    
+    int max_val = pq.top();
+    
+    while(pq.size() > 1) {
         pq.pop();
     }
     
-    if(dq.empty()) {
-        return vector<int>{0, 0};
-    }
-
-    answer.push_back(dq.back());
-    answer.push_back(dq.front());
-    
-    return answer;
+    return vector<int>{max_val, pq.top()};
 }
