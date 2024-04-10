5c9e78f8a846a4bd4589ae5a9b5da9085b421ec1
Yerim Eom <djadpfa1002@naver.com>
2024-04-10 16:50:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -2,43 +2,37 @@
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    priority_queue<int> pq;  // 최대힙
+    priority_queue<int> maxPQ;  // 최대힙
+    priority_queue<int, vector<int>, greater<int>> minPQ;   // 최소힙
+    int size = 0;
     
     for(string op : operations) {
         char cmd = op[0];
         int val = stoi(op.substr(2));
         
         if(cmd == 'I') {    // 삽입
-            pq.push(val);
+            maxPQ.push(val);
+            minPQ.push(val);
+            size++;
         } else {            // 삭제
-            if(pq.empty()) {
+            if(size == 0) {
                 continue;
             }
             
             if(val == 1) {  // 최댓값 삭제
-                pq.pop();
+                maxPQ.pop();
             } else {        // 최솟값 삭제
-                priority_queue<int> tmp;
-                
-                while(pq.size() > 1) {
-                    tmp.push(pq.top());
-                    pq.pop();
-                }
-                
-                pq = tmp;
+                minPQ.pop();
             }
+            
+            size--;
+        }
+        
+        if(size == 0) {
+            maxPQ = priority_queue<int>();
+            minPQ = priority_queue<int, vector<int>, greater<int>>();
         }
     }
-    
-    if(pq.empty()) {
-        return vector<int>{0, 0};
-    }
-    
-    int max_val = pq.top();
-    
-    while(pq.size() > 1) {
-        pq.pop();
-    }
-    
-    return vector<int>{max_val, pq.top()};
+
+    return size == 0 ? vector<int>{0, 0} : vector<int>{maxPQ.top(), minPQ.top()};
 }
