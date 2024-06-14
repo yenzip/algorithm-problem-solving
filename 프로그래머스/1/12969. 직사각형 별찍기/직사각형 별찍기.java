418be296d1b5d4ed7d361ad631122fb62dae5090
Yerim Eom <djadpfa1002@naver.com>
2024-06-14 20:42:42 +0900

    [level 1] Title: 직사각형 별찍기, Time: 141.98 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String star = "*";
+        
+        for(int i = 0; i < m; i++) {
+            System.out.println(star.repeat(n));
+        }
+        
+        sc.close();
+    }
+}
