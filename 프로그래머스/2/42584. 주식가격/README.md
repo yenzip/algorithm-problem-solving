374a0c9137db1b2ab54d8a4342cb40b565e22296
Yerim Eom <djadpfa1002@naver.com>
2024-06-15 17:10:57 +0900

    [level 2] Title: 주식가격, Time: 13.63 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 71.1 MB, 시간: 52.74 ms
+메모리: 62.3 MB, 시간: 13.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 15일 17:07:15
+2024년 06월 15일 17:10:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,18 +5,13 @@ class Solution {
         int[] answer = new int[prices.length];
         
         for(int i = 0; i < prices.length - 1; i++) {
-            Queue<Integer> q = new LinkedList<>();
-            
             for(int j = i + 1; j < prices.length; j++) {
-                q.offer(j);
+                answer[i]++;
                 
                 if(prices[i] > prices[j]) {
                     break;
-                } 
+                }
             }
-            
-            answer[i] = q.size();
-            q.clear();
         }
         
         return answer;
