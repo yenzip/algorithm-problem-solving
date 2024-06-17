d4057abc3902a213015a15fb603bd8770f2e4336
Yerim Eom <djadpfa1002@naver.com>
2024-06-17 14:38:03 +0900

    [level 2] Title: 가장 큰 수, Time: 3.27 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 2.62 ms
+메모리: 77.8 MB, 시간: 3.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 12:03:36
+2024년 06월 17일 14:38:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        List<String> sNumbers = new ArrayList<>();
+        // int 배열을 String 배열로 변환하는 방법: Stream 사용
+        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
         
-        for(int number : numbers) {
-            sNumbers.add(String.valueOf(number));
-        }
+        // 람다식을 사용해 내림차순 사용자 정렬하기
+        // 문자열 비교: compareTo(s1, s2)
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         
-        Collections.sort(sNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = 0; i < sNumbers.size(); i++) {
-            sb.append(sNumbers.get(i));
-        }
-        
-        return sb.charAt(0) == '0' ? "0" : sb.toString();
+        // String 배열을 String으로 변환하기: join(delim, arr)
+        // [Error] String 배열의 모든 원소가 0인 경우 예외 처리하기
+        return arr[0].equals("0") ? "0" : String.join("", arr);
     }
 }
