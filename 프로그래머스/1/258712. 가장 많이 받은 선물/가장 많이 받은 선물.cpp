9d7bf96b9aaf910ddf380dc451fc8fba36e032ba
Yerim Eom <djadpfa1002@naver.com>
2024-07-09 09:50:32 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.35 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int N;
 map<string, int> friend_map;
 int give[50][50];           // give[i][j]: 이번 달 i번 친구가 j번 친구에게 선물을 준 횟수
-int give_n_take[50][2];   // 이번 달 선물을 주고 받은 횟수
-int next_month[50];         // 다음 달 선물을 받은 횟수
+int give_n_take[50];        // 선물 지수: 이번 달 선물을 주고 받은 횟수(주면 ++, 받으면 --)
+int next_month[50];         // 다음 달 선물을 받을 횟수
 
 int solution(vector<string> friends, vector<string> gifts) {    
     N = friends.size();
@@ -23,29 +23,26 @@ int solution(vector<string> friends, vector<string> gifts) {
         int t = friend_map[taker];
         
         give[g][t]++;
-        give_n_take[g][0]++;
-        give_n_take[t][1]++;
+        give_n_take[g]++;
+        give_n_take[t]--;
     }
     
-    for(int i = 0; i < N; i++) {    //  다음 달 받을 선물 예측하기
+    for(int i = 0; i < N; i++) {
         for(int j = i + 1; j < N; j++) {
-            if(give[i][j] > give[j][i]) {           // 다음 달 B가 A에게 선물 주기
+            if(give[i][j] > give[j][i]) {
                 next_month[i]++;
             } else if(give[i][j] < give[j][i]) {    // 다음 달 A가 B에게 선물 주기
                 next_month[j]++;
-            } else {                          
-                int A = give_n_take[i][0] - give_n_take[i][1];  // A의 선물지수
-                int B = give_n_take[j][0] - give_n_take[j][1];  // B의 선물지수
-                
-                if(A > B) {
+            } else {
+                if(give_n_take[i] > give_n_take[j]) {
                     next_month[i]++;
-                } else if(A < B) {
+                } else if(give_n_take[i] < give_n_take[j]) {
                     next_month[j]++;
                 }
             }
         }
     }
-    
+
     int answer = *max_element(next_month, next_month + N);
     
     return answer;
