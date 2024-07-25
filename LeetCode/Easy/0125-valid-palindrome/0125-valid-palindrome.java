b473d746eda710cccc3003aa848ca650e06dbd14
Yerim Eom <djadpfa1002@naver.com>
2024-07-25 15:45:39 +0900

    [Easy] Time: 15 ms (25.89%), Memory: 44.7 MB (32.45%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        s = s.toUpperCase();
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i <= j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
