1d7190e0d7351da7d0615d283965941e95fbc6ce
Yerim Eom <djadpfa1002@naver.com>
2024-07-25 20:01:37 +0900

    [Easy] Time: 0 ms (100.00%), Memory: 45.7 MB (70.98%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
@@ -1,10 +1,19 @@
+// 투포인터 풀이
+
 class Solution {
     public void reverseString(char[] s) {
-        String tmp = String.valueOf(s);
         int len = s.length;
+        int start = 0;
+        int end = len - 1;
         
-        for(int i = 0; i < len; i++) {
-            s[i] = tmp.charAt(len - i - 1);
+        while(start < end) {
+            char tmp = s[start];    // Java는 swap 함수 존재하지 않음
+            s[start] = s[end];
+            s[end] = tmp;
+            
+            start++;
+            end--;
         }
+        
     }
 }
