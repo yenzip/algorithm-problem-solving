edd12f31e26afcc090dc3a0dd5b1db5fcd08a4bc
Yerim Eom <djadpfa1002@naver.com>
2024-08-01 17:13:40 +0900

    [Easy] Time: 15 ms (25.86%), Memory: 45.1 MB (21.40%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,16 +2,8 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]+", "");
         
-        int i = 0;
-        int len = s.length();
+        String reverse = new StringBuilder(s).reverse().toString();
         
-        while(i < len / 2) {
-            if(s.charAt(i) != s.charAt(len - i - 1)) {
-                return false;
-            }
-            i++;
-        }
-        
-        return true;
+        return s.equals(reverse);
     }
 }
