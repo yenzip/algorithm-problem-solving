caa85681189296854659bfb284da3bd14d6d33db
Yerim Eom <djadpfa1002@naver.com>
2024-08-03 21:20:11 +0900

    [Gold V] Title: 회문, Time: 312 ms, Memory: 34676 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -3,48 +3,50 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         int T = Integer.parseInt(br.readLine());
-        
+
         for(int i = 0; i < T; i++) {
             String s = br.readLine();
-            System.out.println(checkPalindrome(s.toCharArray()));
+            sb.append(checkPalindrome(s.toCharArray())).append('\n');
         }
         
+        System.out.println(sb);
+
         br.close();
     }
-    
+
     private static int checkPalindrome(char[] s) {
         int len = s.length;
-        int i = 0, j = len - 1;
-        
+        int i = 0;
+        int j = len - 1;
+
         while(i < j) {
             if(s[i] == s[j]) {
                 i++;
                 j--;
-            } else {
-                int tmpI = i + 1, tmpJ = j;
-                while(tmpI <= tmpJ && s[tmpI] == s[tmpJ]) {
-                    tmpI++;
-                    tmpJ--;
-                }
-                if(tmpI > tmpJ) {
-                    return 1;
-                }
-                
-                tmpI = i; tmpJ = j - 1;
-                while(tmpI <= tmpJ && s[tmpI] == s[tmpJ]) {
-                    tmpI++;
-                    tmpJ--;
-                }
-                if(tmpI > tmpJ) {
-                    return 1;
-                }
-                
-                return 2;
+                continue;
+            } 
+            
+            // 유사회문인지 확인
+            if(isSimilarPalindrome(s, i + 1, j) || isSimilarPalindrome(s, i, j - 1)) {
+                return 1;
             }
+            
+            // 일반 문자열
+            return 2;
+        }
+
+        return 0;
+    }
+    
+    private static boolean isSimilarPalindrome(char[] s, int i, int j) {
+        while(i <= j && s[i] == s[j]) {
+            i++;
+            j--;
         }
         
-        return 0;
+        return i > j;
     }
 }
