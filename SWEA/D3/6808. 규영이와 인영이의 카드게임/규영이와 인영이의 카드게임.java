7cf697a4b6cd6f422c693b7f33979e99c0a350ba
Yerim Eom <djadpfa1002@naver.com>
2024-08-13 15:27:43 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 3,380 ms, Memory: 20,804 KB -BaekjoonHub

---
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static boolean[] card;	// 규영이 카드인지 확인
+	private static int[] kyuyoung;
+	private static int[] inyoung;
+	private static int[] perm;
+	private static boolean[] isSelected;
+	private static int win, loose;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+            
+			card = new boolean[19];
+			kyuyoung = new int[9];
+			inyoung = new int[9];
+			perm = new int[9];
+			isSelected = new boolean[9];
+			win = 0;
+			loose = 0;
+			
+			for(int i = 0; i < 9; i++) {
+				int number = Integer.parseInt(st.nextToken());
+				kyuyoung[i] = number;
+				card[number] = true;
+			}
+			
+			int index = 0;
+			for(int i = 1; i <= 18; i++) {
+				if(!card[i]) {
+					inyoung[index++] = i;
+				}
+			}
+			
+			permutation(0);
+			
+			System.out.println("#" + tc + " " + win + " " + loose);
+		}
+	}
+	
+	private static void permutation(int depth) {
+		if(depth == 9) {
+			if(isKyuyoungWin()) {
+				win++;
+			} else {
+				loose++;
+			}
+			return;
+		}
+		
+		for(int i = 0; i < 9; i++) {
+			if(!isSelected[i]) {
+				isSelected[i] = true;
+				perm[depth] = inyoung[i];
+				permutation(depth + 1);
+				isSelected[i] = false;
+			}
+		}
+	}
+	
+	private static boolean isKyuyoungWin() {
+		int kyu = 0;
+		int in = 0;
+		for(int i = 0; i < 9; i++) {
+			if(kyuyoung[i] > perm[i]) {
+				kyu += kyuyoung[i] + perm[i];
+			} else if(kyuyoung[i] < perm[i]) {
+				in += kyuyoung[i] + perm[i];
+			}
+		}
+		return kyu > in;
+	}
+
+}
